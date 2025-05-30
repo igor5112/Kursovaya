159a7ebc4ad8395dfb969edc812fb0c73b3380c9
igor5112 <145256321+igor5112@users.noreply.github.com>
2025-05-30 22:29:24 +0300

    Update SystemController.cpp

--- SystemController.cpp
+++ SystemController.cpp
@@ -3,7 +3,7 @@
 SystemController::SystemController(const std::vector<Classroom>& rooms)
     : roomSearcher(rooms) // Здесь только инициализируется roomSearcher
 {
-    // Никаких попыток создать ScheduleManager — он получаем через getInstance()
+    
 }
 
 std::vector<Classroom> SystemController::getFreeRooms(const std::string &date, const TimeSlot &timeSlot) const {
